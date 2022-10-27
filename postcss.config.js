f26baeabc680355f951d45d0ae4f6c00c583d3b1
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-27 16:40:24 +0800

    tested out plugins

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,8 @@ module.exports = {
         require('autoprefixer'),
         require('postcss-preset-env')({
             stage: 1
-        })
+        }),
+        require('precss'),
+        require('postcss-import')
     ]
 }
